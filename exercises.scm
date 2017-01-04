3120e3cfa49dc48d6c55af9e070aef31c70a7b58
Kyumin Kim <math4tots@gmail.com>
2017-01-04 02:52:42 -0800

    WIP

--- exercises.scm
+++ exercises.scm
@@ -23,3 +23,34 @@
 
 ; Applicative-order -> infinite loop
 ; Normal-order -> return zero
+
+;; sec 1.3
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+; Exercise 1.29
+; integral using Simpson's rule
+(define (exercise-1.29 f a b n)
+  (define h (/ (- b a) n))
+  (define (y k) (f (+ a (* k h))))
+  (define (rec total i)
+    (if (> a b)
+        0
+        ()))
+  )
+
+; Exercise 1.30
+; Iterative version of 'sum'
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        0
+        (iter (next a)
+              (+ ))))
+  (iter a 0))
+
+
